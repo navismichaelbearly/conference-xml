402850d4f898f96a0f72ab293d82fc467c2c248a
NBEARLYJ <nbearlyj@ford.com>
2023-02-23 21:03:02 +0530

    implemented setter injection with xml configuration

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,17 @@
 import in.bearly.service.SpeakerService;
 import in.bearly.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
     public static void main(String[] args) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+
+        //SpeakerService service = new SpeakerServiceImpl();
+
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.println(service.findAll().get(0).getFirstName());
     }
--- src/main/java/in/bearly/service/SpeakerServiceImpl.java
+++ src/main/java/in/bearly/service/SpeakerServiceImpl.java
@@ -1,17 +1,20 @@
 package in.bearly.service;
 
 import in.bearly.model.Speaker;
-import in.bearly.repository.HibernateSpeakerRepositoryImpl;
 import in.bearly.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositoryImpl();
+    private SpeakerRepository repository;
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
